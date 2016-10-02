6b768fe90dd7cf33eda5691a8304bd901a2b906b
Alex Briggs <briggs50@mail.nmc.edu>
2016-10-02 12:43:21 -0400

    Started to build out a historic stats txt and cs' based off the demo from class

---
+++ CodingActivity_TicTacToe_ConsoleGame.Solution/Model/HistoricScore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_TicTacToe_ConsoleGame.Model
+{
+    class HistoricScore
+    {
+        public int roundsPlayed { get; set; }
+        public int playerXWins { get; set; }
+        public int playerOWins { get; set; }
+        public int catsGames { get; set; }
+    }
+}
