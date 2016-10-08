20a25316358513a593c6c3393b4458a2531abcac
Alex Briggs <briggs50@mail.nmc.edu>
2016-10-08 13:30:20 -0400

    Made an exception class

---
+++ CodingActivity_TicTacToe_ConsoleGame.Solution/Controller/GameException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_TicTacToe_ConsoleGame.Controller
+{
+    //Inherit from main Exception class
+    public class PositionChoiceOutOfRangeException : Exception
+    {
+        //use this constructor to set our own message.
+        public PositionChoiceOutOfRangeException(string message) : base(message)
+        {
+        }
+    }
+
+    public class PoisitionChoiceAlreadyTakenException : Exception
+    {
+        public PoisitionChoiceAlreadyTakenException(string message) : base(message)
+        {
+        }
+    }
+}
