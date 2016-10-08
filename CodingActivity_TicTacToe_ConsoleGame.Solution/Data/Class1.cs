4d9737f7ba5f3f714a3d1ff880d6ad4cf087286d
Jen Berigan <jenberigan@gmail.com>
2016-10-08 14:30:51 -0400

    added data folder and started on displaying historic scores

---
+++ CodingActivity_TicTacToe_ConsoleGame.Solution/Data/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_TicTacToe_ConsoleGame.Data
+{
+    public class DataSettings
+    {
+        public const string DataFilePath = "Data\\PlayerHistory.txt";
+        public const char Delineator = ',';
+    }
+}
---
+++ CodingActivity_TicTacToe_ConsoleGame.Solution/Model/PlayerScores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_TicTacToe_ConsoleGame.Model
+{
+    public class PlayerScores
+    {
+       public string PlayerName { get; set; }
+       public int PlayerScore { get; set; }
+    }
+}
